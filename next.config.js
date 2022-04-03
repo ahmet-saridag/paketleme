f2c3a595c7b37dff05af1a8e835f0de03bbfa973
Ahmet Sarıdag <ahmet.saridag@globalservs.com>
2022-04-03 21:29:23 +0300

    Last

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfiguration = {
   future: {
     webpack5: true,
   },
-  target: 'static', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+  target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
 };
 
 module.exports = withPlugins([optimizedImages], nextConfiguration);
